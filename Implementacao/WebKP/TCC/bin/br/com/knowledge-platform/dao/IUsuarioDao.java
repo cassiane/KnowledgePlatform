aade5489ca4aa43a0b844ce553d93c3bb49217f8
Cassiane Santos <cassianeribasdasilva@gmail.com>
2017-08-11 09:42:02 -0300

    Ajuste de projeto

--- Implementacao/WebKP/TCC/bin/br/com/knowledge-platform/dao/IUsuarioDao.java
+++
@@ -1,9 +0,0 @@
-package br.com.knowl.platform.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.knowl.platform.modelo.Usuario;
-
-public interface IUsuarioDao extends  CrudRepository<Usuario, Integer> {
-	Usuario findByEmail(String email);
-}
